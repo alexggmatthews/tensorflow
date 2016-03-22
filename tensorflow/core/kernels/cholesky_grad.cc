1ebfe2fb894af93166ea40ba765ad1d17b81f7f3
alexggmatthews <alexggmatthews@googlemail.com>
2016-03-22 16:22:27 +0000

    Editing amounts of whitespace in line with google open source coding standard.

--- tensorflow/core/kernels/cholesky_grad.cc
+++ tensorflow/core/kernels/cholesky_grad.cc
@@ -27,104 +27,86 @@ namespace tensorflow {
 
 template <typename T>
 class CholeskyGrad : public OpKernel {
-    public:
-    
-    explicit CholeskyGrad(OpKernelConstruction* context) : OpKernel(context) {}
+ public:
+  explicit CholeskyGrad(OpKernelConstruction* context) : OpKernel(context) {}
+  using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+  using ConstMatrixMap = Eigen::Map<const Matrix>;
+  using MatrixMap = Eigen::Map<Matrix>;
+  using ConstRef = Eigen::Ref<const Matrix>;
+  using Ref = Eigen::Ref<Matrix>;
 
-    using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-    using ConstMatrixMap = Eigen::Map<const Matrix>;
-    using MatrixMap = Eigen::Map<Matrix>;
-    using ConstRef = Eigen::Ref<const Matrix>;
-    using Ref = Eigen::Ref<Matrix>;
-
-    void Compute(OpKernelContext* context) override {
-
-    const Tensor& input_tensor_l = context->input(0); ;
-    const Tensor& input_tensor_grad = context->input(1); 
-
-    //Check that input tensors represent a matrix.
+  void Compute(OpKernelContext* context) override {
+    const Tensor& input_tensor_l = context->input(0);
+    const Tensor& input_tensor_grad = context->input(1);
+    // Check that input tensors represent a matrix.
     OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_l.shape()), errors::InvalidArgument("In[0] is not a matrix"));
     OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_grad.shape()), errors::InvalidArgument("In[1] is not a matrix"));
-
-    //Check that input tensors are square.
+    // Check that input tensors are square.
     OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_l.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
     OP_REQUIRES(context, input_tensor_grad.dim_size(0) == input_tensor_grad.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
 
-    //Check that input tensors are of same size.
-    OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_grad.dim_size(0), errors::InvalidArgument("Input matrices must be of same size."));    
+    // Check that input tensors are of same size.
+    OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_grad.dim_size(0), errors::InvalidArgument("Input matrices must be of same size."));
 
     // Create an output tensor
     Tensor* output_tensor = NULL;
-    
     OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor_grad.shape(), &output_tensor));
 
     if (output_tensor->NumElements() == 0) {
       // the output shape is a 0-element matrix, so there is nothing to do.
       return;
     }
-    
-    //The next three lines are necessary to get Eigen matrix behaviour.
+    // The next three lines are necessary to get Eigen matrix behaviour.
     const ConstMatrixMap input_matrix_l(input_tensor_l.flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1));
     const ConstMatrixMap input_matrix_grad(input_tensor_grad.flat<T>().data(), input_tensor_grad.dim_size(0), input_tensor_grad.dim_size(1));
-    MatrixMap output_matrix(output_tensor->template flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1) );    
+    MatrixMap output_matrix(output_tensor->template flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1) );
 
     const int64 kMatrixSize = input_matrix_l.rows();
     const int64 kMaxBlockSize = 32;
 
     output_matrix = input_matrix_grad.template triangularView<Eigen::Lower>();
-
-    
-    for ( int64 block_end = kMatrixSize ; block_end>0ll; block_end-= kMaxBlockSize )   
-    {
-        const int64 block_begin = std::max(0ll, block_end - kMaxBlockSize);
-        const int64 block_size = block_end - block_begin;
-        const int64 trailing_size = kMatrixSize - block_size ; 
-    
-        output_matrix.block(block_end, block_begin, trailing_size , block_size) = input_matrix_l.block(block_begin, block_begin, block_size, block_size)
+    for ( int64 block_end = kMatrixSize ; block_end > 0ll; block_end-= kMaxBlockSize ) {
+      const int64 block_begin = std::max(0ll, block_end - kMaxBlockSize);
+      const int64 block_size = block_end - block_begin;
+      const int64 trailing_size = kMatrixSize - block_size;
+      output_matrix.block(block_end, block_begin, trailing_size , block_size) = input_matrix_l.block(block_begin, block_begin, block_size, block_size)
                                                                                                  .adjoint()
                                                                                                  .template triangularView<Eigen::Upper>()
                                                                                                  .solve(output_matrix.block(block_end, block_begin, trailing_size, block_size)
                                                                                                  .adjoint() )
                                                                                                  .adjoint();
 
-        output_matrix.block(block_begin, block_begin, block_size, block_size) -= (output_matrix.block(block_end, block_begin, trailing_size, block_size)
-                                                                                                 .adjoint() 
+      output_matrix.block(block_begin, block_begin, block_size, block_size) -= (output_matrix.block(block_end, block_begin, trailing_size, block_size)
+                                                                                                 .adjoint()
                                                                                                  * input_matrix_l.block(block_end, block_begin, trailing_size, block_size))
                                                                                                  .template triangularView<Eigen::Lower>();
-        output_matrix.block(block_end, 0, trailing_size, block_begin)  -=  output_matrix.block(block_end, block_begin, trailing_size, block_size) 
+      output_matrix.block(block_end, 0, trailing_size, block_begin)  -=  output_matrix.block(block_end, block_begin, trailing_size, block_size)
                                                                                           * input_matrix_l
-                                                                                          .block(block_begin, 0, block_size, block_begin); 
+                                                                                          .block(block_begin, 0, block_size, block_begin);
 
-        output_matrix.block(block_begin, 0, block_size, block_begin) -= output_matrix.block(block_end, block_begin, trailing_size, block_size)
-                                                                                      .adjoint() 
+      output_matrix.block(block_begin, 0, block_size, block_begin) -= output_matrix.block(block_end, block_begin, trailing_size, block_size)
+                                                                                      .adjoint()
                                                                                       * input_matrix_l.block(block_end, 0, trailing_size, block_begin);
-        CholeskyGradUnblocked(input_matrix_l.block(block_begin, block_begin, block_size, block_size), output_matrix
-                                          .block( block_begin, block_begin, block_size, block_size));
-        output_matrix.block(block_begin, 0, block_size, block_begin) -= (output_matrix.block(block_begin, block_begin, block_size, block_size) 
+      CholeskyGradUnblocked(input_matrix_l.block(block_begin, block_begin, block_size, block_size), output_matrix
+                                          .block(block_begin, block_begin, block_size, block_size));
+      output_matrix.block(block_begin, 0, block_size, block_begin) -= (output_matrix.block(block_begin, block_begin, block_size, block_size)
                                                                                         +output_matrix.block(block_begin, block_begin, block_size, block_size)
                                                                                         .adjoint() )
                                                                                         * input_matrix_l
                                                                                         .block(block_begin, 0, block_size, block_begin);
     }
-    
-    output_matrix = (0.5 * ( output_matrix +  output_matrix.transpose() )).eval();
-
+    output_matrix = (0.5 * (output_matrix +  output_matrix.transpose())).eval();
+  }
+  void CholeskyGradUnblocked(const ConstRef l_block, Ref grad_block) {
+    const int64 kMatrixSize = l_block.rows();
+    for (int64 k = kMatrixSize-1; k >= 0; k--) {
+      grad_block(k, k) -= (l_block.block(k+1, k, kMatrixSize-(k+1), 1).adjoint() * grad_block.block(k+1, k, kMatrixSize-(k+1), 1))(0, 0) / l_block(k, k);
+      grad_block.block(k, k, kMatrixSize-k, 1) /= l_block(k, k);
+      grad_block.block(k, 0, 1, k) -= grad_block.block(k, k, kMatrixSize-k, 1).adjoint() * l_block.block(k, 0, kMatrixSize-k, k);
+      grad_block.block(k+1, 0, kMatrixSize-(k+1), k) -= grad_block.block(k+1, k , kMatrixSize-(k+1), 1) * l_block.block(k, 0, 1, k);
+      grad_block(k, k) *= 0.5;
     }
-    
-    void CholeskyGradUnblocked(const ConstRef l_block, Ref grad_block)
-    {
-        const int64 kMatrixSize = l_block.rows();
-        
-        for ( int64 k = kMatrixSize-1; k>=0; k--)
-        {
-            grad_block(k,k) -= (l_block.block(k+1,k , kMatrixSize-(k+1), 1).adjoint() * grad_block.block(k+1,k , kMatrixSize-(k+1), 1))(0,0) / l_block(k,k);
-            grad_block.block(k,k,kMatrixSize-k,1) /= l_block( k,k) ;
-            grad_block.block(k,0,1,k) -=grad_block.block(k,k,kMatrixSize-k,1).adjoint() * l_block.block(k,0,kMatrixSize-k,k);
-            grad_block.block(k+1,0,kMatrixSize-(k+1),k) -= grad_block.block(k+1, k , kMatrixSize-(k+1), 1) * l_block.block(k, 0, 1, k) ; 
-            grad_block(k,k) *= 0.5;
-        }
-    }
-    
+  }
 };
 
 REGISTER_LINALG_OP("CholeskyGrad", (CholeskyGrad<float>), float);
