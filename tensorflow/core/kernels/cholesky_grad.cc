857d44b0d04a74eaff3fd3ed84183e599125133d
alexggmatthews <alexggmatthews@googlemail.com>
2016-03-22 13:08:49 +0000

    Giving input variables clearer names.

--- tensorflow/core/kernels/cholesky_grad.cc
+++ tensorflow/core/kernels/cholesky_grad.cc
@@ -40,23 +40,23 @@ class CholeskyGrad : public OpKernel {
     void Compute(OpKernelContext* context) override {
 
     const Tensor& input_tensor_l = context->input(0); ;
-    const Tensor& input_tensor_l_bar = context->input(1); 
+    const Tensor& input_tensor_grad = context->input(1); 
 
     //Check that input tensors represent a matrix.
     OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_l.shape()), errors::InvalidArgument("In[0] is not a matrix"));
-    OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_l_bar.shape()), errors::InvalidArgument("In[1] is not a matrix"));
+    OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_grad.shape()), errors::InvalidArgument("In[1] is not a matrix"));
 
     //Check that input tensors are square.
     OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_l.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
-    OP_REQUIRES(context, input_tensor_l_bar.dim_size(0) == input_tensor_l_bar.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
+    OP_REQUIRES(context, input_tensor_grad.dim_size(0) == input_tensor_grad.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
 
     //Check that input tensors are of same size.
-    OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_l_bar.dim_size(0), errors::InvalidArgument("Input matrices must be of same size."));    
+    OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_grad.dim_size(0), errors::InvalidArgument("Input matrices must be of same size."));    
 
     // Create an output tensor
     Tensor* output_tensor = NULL;
     
-    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor_l_bar.shape(), &output_tensor));
+    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor_grad.shape(), &output_tensor));
 
     if (output_tensor->NumElements() == 0) {
       // the output shape is a 0-element matrix, so there is nothing to do.
@@ -65,13 +65,13 @@ class CholeskyGrad : public OpKernel {
     
     //The next three lines are necessary to get Eigen matrix behaviour.
     const ConstMatrixMap input_matrix_l(input_tensor_l.flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1));
-    const ConstMatrixMap input_matrix_l_bar(input_tensor_l_bar.flat<T>().data(), input_tensor_l_bar.dim_size(0), input_tensor_l_bar.dim_size(1));
+    const ConstMatrixMap input_matrix_grad(input_tensor_grad.flat<T>().data(), input_tensor_grad.dim_size(0), input_tensor_grad.dim_size(1));
     MatrixMap output_matrix(output_tensor->template flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1) );    
 
     const int64 N = input_matrix_l.rows();
     const int64 kMaxBlockSize = 32;
 
-    output_matrix = input_matrix_l_bar.template triangularView<Eigen::Lower>();
+    output_matrix = input_matrix_grad.template triangularView<Eigen::Lower>();
 
     
     for ( int64 block_end = N ; block_end>0ll; block_end-= kMaxBlockSize )   
@@ -111,17 +111,17 @@ class CholeskyGrad : public OpKernel {
 
     }
     
-    void chol_rev_unblocked(const ConstRef l_block, Ref l_bar_block)
+    void chol_rev_unblocked(const ConstRef l_block, Ref grad_block)
     {
         const int64 N = l_block.rows();
         
         for ( int64 k = N-1; k>=0; k--)
         {
-            l_bar_block(k,k) -= (l_block.block( k+1,k , N-(k+1), 1 ).adjoint() * l_bar_block.block( k+1,k , N-(k+1), 1 ) )(0,0) / l_block(k,k);
-            l_bar_block.block(k,k,N-k,1) /= l_block( k,k) ;
-            l_bar_block.block(k,0,1,k) -=l_bar_block.block(k,k,N-k,1).adjoint() * l_block.block(k,0,N-k,k);
-            l_bar_block.block(k+1,0,N-(k+1),k) -= l_bar_block.block(k+1, k , N-(k+1), 1 ) * l_block.block( k, 0, 1, k ) ; 
-            l_bar_block(k,k) *= 0.5;
+            grad_block(k,k) -= (l_block.block( k+1,k , N-(k+1), 1 ).adjoint() * grad_block.block( k+1,k , N-(k+1), 1 ) )(0,0) / l_block(k,k);
+            grad_block.block(k,k,N-k,1) /= l_block( k,k) ;
+            grad_block.block(k,0,1,k) -=grad_block.block(k,k,N-k,1).adjoint() * l_block.block(k,0,N-k,k);
+            grad_block.block(k+1,0,N-(k+1),k) -= grad_block.block(k+1, k , N-(k+1), 1 ) * l_block.block( k, 0, 1, k ) ; 
+            grad_block(k,k) *= 0.5;
         }
     }
     
